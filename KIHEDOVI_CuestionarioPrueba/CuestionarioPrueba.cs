0ae03d929daaf7d6d94c742db68a0d5bca065b0a
KEVIN ANTHONY CASACHAGUA, LLACZA <70684903@continental.edu.pe>
2020-12-09 05:40:01 -0500

    Cronometro, Form Responder y Documentacion

--- KIHEDOVI_CuestionarioPrueba/CuestionarioPrueba.cs
+++ KIHEDOVI_CuestionarioPrueba/CuestionarioPrueba.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KIHEDOVI_Cuestionario;
 
 namespace KIHEDOVI_CuestionarioPrueba
 {
     [TestClass]
     public class CuestionarioPrueba
     {
+        /// <summary>Pruebas the titulo.</summary>
         [TestMethod]
-        public void Cuestionario_Prueba()
+        public void Prueba_Titulo()
         {
+            //Arranque
+            String titulo = "matematica";
+            Boolean rpta;
+            KIHEDOVI_Cuestionario.FormCrearCuestionario tl = new KIHEDOVI_Cuestionario.FormCrearCuestionario();
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(true, titulo);
+        }
+
+        /// <summary>Pruebas the codigo.</summary>
+        [TestMethod]
+        public void Prueba_Codigo()
+        {
+            //Arranque
+            String codigo = "matematica";
+            Boolean rpta;
+            KIHEDOVI_Cuestionario.FormCrearCuestionario cod = new KIHEDOVI_Cuestionario.FormCrearCuestionario();
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(true, codigo);
+        }
+
+        /// <summary>Pruebas the pregunta.</summary>
+        public void Prueba_Pregunta()
+        {
+            //Arranque
+            String codigo = "matematica";
+            Boolean rpta;
+            KIHEDOVI_Cuestionario.FormCrearCuestionario cod = new KIHEDOVI_Cuestionario.FormCrearCuestionario();
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(true, codigo);
         }
     }
 }
